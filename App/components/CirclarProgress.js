0c3f73c7fb8b08835776b8c2d444e5472eaaddf7
Evan Uwao <uwaoevanz@gmail.com>
2022-02-23 10:42:47 +0300

    Configured the progress bar

--- App/components/CirclarProgress.js
+++ App/components/CirclarProgress.js
@@ -1,30 +1,51 @@
 import React from "react";
-import Svg, { Circle } from "react-native-svg";
-import { Dimensions, View } from 'react-native'
-import { COLORS } from "../constants/colors";
+import { Dimensions, View, Text, StyleSheet } from 'react-native'
+import { ProgressChart } from "react-native-chart-kit";
+
 
 export default function CircularProgress () {
-    const { width, height } = Dimensions.get('screen')
-    let circleLength = 10
-    let r = circleLength * (2*Math.PI)
+    const data = {
+        labels: ["Mental Health", "Satisfaction", "Family/Social Support", "Work", "Sence of Purpose"], // optional
+        data: [0.4, 0.6, 0.8, 0.3, 0.5],
+        colors: ['rgba(20, 48, 41)', 'rgba(133, 189, 175)', 'rgba(189, 217, 210)', 'rgba(227, 176, 159)', 'rgba(246, 233, 231)']
+      };
+    const chartConfig = {
+        backgroundGradientFrom: "#fff",
+        backgroundGradientFromOpacity: 0,
+        backgroundGradientTo: "#fff",
+        backgroundGradientToOpacity: 0.5,
+        color: (opacity = 1) => `rgba(210, 210, 210, ${opacity})`,
+        strokeWidth: 2,
+        useShadowColorFromDataset: false 
+    };
+
+    const { width } = Dimensions.get('screen')
     return(
-        <Svg style={{ height: width / 2.5}}>
-            <Circle
-                cx={width / 5}
-                cy={height / 10}
-                r={r}
-                stroke={COLORS.primary}
-                strokeWidth={10}
-            />
-            <Circle
-                cx={width / 5}
-                cy={height / 10}
-                r={r}
-                stroke={COLORS.secondary}
+        <View style={styles.cont}>
+        <ProgressChart
+                data={data}
+                width={width * 0.8}
+                height={150}
                 strokeWidth={5}
-                // strokeDasharray={circleLength}
-                // strokeDashoffset={circleLength / 2}
-            />
-        </Svg>
+                radius={25}
+                chartConfig={chartConfig}
+                withCustomBarColorFromData={data.colors}
+                style={{ marginLeft: - width / 10 }}
+        />
+        <Text style={styles.number}>50</Text>
+       </View>
     )
 } 
+
+const styles = StyleSheet.create({
+    cont:{
+        position: 'relative'
+    },
+    number: {
+        fontFamily: 'Raleway-Bold',
+        fontSize: 30,
+        position: 'absolute',
+        top: '35%',
+        left: '22%'
+    }
+})
